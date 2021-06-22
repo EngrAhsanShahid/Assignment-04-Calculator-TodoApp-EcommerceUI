5a8004a650f010f7a5e188e5a2f994b48554a9cd
EngrAhsanShahid <EngrAhsanShahid@users.noreply.github.com>
2021-06-22 22:23:27 +0500

    add todoapp

--- calculator_todoapp_ecommerceui/lib/todoapp.dart
+++ calculator_todoapp_ecommerceui/lib/todoapp.dart
@@ -1,8 +1,102 @@
 import 'package:flutter/material.dart';
 
-class TodoAPP extends StatelessWidget {
+class TodoAPP extends StatefulWidget {
+  @override
+  _TodoAPPState createState() => _TodoAPPState();
+}
+
+class _TodoAPPState extends State<TodoAPP> {
+  var output = "";
+  List<dynamic> lst = [1, 2, 3, 4, 5];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Assignment #04"),
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: lst.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 50,
+              color: Colors.amber,
+              margin: EdgeInsets.only(top: 15),
+              child: ListTile(
+                title: Text("${lst[index]}"),
+                trailing: Container(
+                  width: 50,
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text("Edit item"),
+                                    content: TextField(
+                                      onChanged: (value) {
+                                        output = value;
+                                      },
+                                    ),
+                                    actions: [
+                                      ElevatedButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              lst.replaceRange(
+                                                  index, index + 1, {output});
+                                            });
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text("Edit"))
+                                    ],
+                                  );
+                                });
+                          },
+                          child: Icon(Icons.edit)),
+                      GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              lst.removeAt(index);
+                            });
+                          },
+                          child: Icon(Icons.delete)),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Add item"),
+                  content: TextField(
+                    onChanged: (value) {
+                      output = value;
+                    },
+                  ),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            lst.add(output);
+                          });
+                          Navigator.pop(context);
+                        },
+                        child: Text("Add"))
+                  ],
+                );
+              });
+        },
+        child: Text("Add"),
+      ),
+    );
   }
 }
